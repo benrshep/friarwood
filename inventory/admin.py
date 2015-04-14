80ee5f28172827411378931c7716c8699ea13fb7
Ben Shepherd <benshep@me.com>
2015-04-14 16:35:25 +0100

    Added editable fields

--- inventory/admin.py
+++ inventory/admin.py
@@ -4,14 +4,35 @@ from django.db import models
 from .models import Wine, Producer, Varietal, Appellation
 #from wines.tasks import find_wine
 
-from import_export import resources
 from import_export.admin import ImportExportActionModelAdmin
 
+#Change admin site titles
+admin.sites.AdminSite.site_header = 'Friarwood Fine Wines'
+admin.sites.AdminSite.site_title = 'Friarwood'
+
+#Resources
+
+from import_export import resources
 
 class WineResource(resources.ModelResource):
-
     class Meta:
         model = Wine
+
+#Forms
+
+from django import forms
+
+class WineForm(forms.ModelForm):
+    class Meta:
+        model = Wine
+        fields = ['short_name', 'cost_price_s', 'retail_price_s', 'wholesale_price_s']
+        widgets = {
+        'short_name': TextInput(attrs={'size':'100'}),
+        'cost_price_s': TextInput(attrs={'size':'5'}),
+        'retail_price_s': TextInput(attrs={'size':'5'}),
+        'wholesale_price_s': TextInput(attrs={'size':'5'}),
+        'single_size': TextInput(attrs={'size':'5'}),
+        }
 
 # Register your models here.
 
@@ -28,7 +49,6 @@ class AppellationAdmin(admin.ModelAdmin):
 	list_display = ['name','country']
 
 class WineAdmin(ImportExportActionModelAdmin):
-	formfield_overrides = { models.CharField: {'widget': TextInput(attrs={'size':'10'}) } }
 	resource_class = WineResource
 	fieldsets = (
 		(None, {
@@ -48,8 +68,11 @@ class WineAdmin(ImportExportActionModelAdmin):
 	search_fields = ['short_name' ,'vintage', 'single_size']
 	list_per_page = 500
 	list_display_links = ['short_name']
-	list_editable = ['cost_price_s', 'retail_price_s', 'wholesale_price_s']
+	list_editable = ['single_size','cost_price_s', 'retail_price_s', 'wholesale_price_s']
 	readonly_fields = ('sage_name','sage_ref',)
+
+	def get_changelist_form(self, request, **kwargs):
+		return WineForm
 
 	def save_model(self, request, obj, form, change):
 		#Save initial required infomation
