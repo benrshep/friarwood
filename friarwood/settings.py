590332d52d6afba32c070cca85598313c9bb364f
Ben Shepherd <benshep@me.com>
2015-04-14 13:34:25 +0100

    Added debug

--- friarwood/settings.py
+++ friarwood/settings.py
@@ -133,3 +133,5 @@ if not DEBUG:
    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
    STATIC_URL = S3_URL
+
+DEBUG = True
