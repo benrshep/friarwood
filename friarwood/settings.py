c61a790d9171366f782cf2c7de794d37de481325
Ben Shepherd <benshep@me.com>
2015-04-15 09:02:29 +0100

    adjusted database

--- friarwood/settings.py
+++ friarwood/settings.py
@@ -80,14 +80,7 @@ WSGI_APPLICATION = 'friarwood.wsgi.application'
 
 DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'friarwood',                      # Or path to database file if using sqlite3.
-            # The following settings are not used with sqlite3:
-            'USER': 'friarwood',
-            'PASSWORD': 'friarwood',
-            'HOST': '52.16.41.189',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
-            'PORT': '6432',                      # Set to empty string for default.
-        }
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', 
     }
 
 
@@ -116,11 +109,11 @@ STATIC_URL = '/static/'
 ALLOWED_HOSTS = ['friarwood.herokuapp.com']
 
 # Parse database configuration from $DATABASE_URL
-'''
+
 import dj_database_url
 
 DATABASES = { 'default' : dj_database_url.config()}
-'''
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
