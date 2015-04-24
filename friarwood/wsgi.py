777bfa988768c62647d302dea05d8372d1ce6ab9
Ben Shepherd <benshep@me.com>
2015-04-24 11:47:03 +0100

    server adjustments

--- friarwood/wsgi.py
+++ friarwood/wsgi.py
@@ -6,8 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
+
+
 #Heroku
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
@@ -18,5 +19,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "friarwood.settings")
 
 #Heroku
 application = Cling(get_wsgi_application())
-
-
