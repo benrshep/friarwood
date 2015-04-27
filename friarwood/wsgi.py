6c47e7e91691427e299b95902c738181c5ae602d
Ben Shepherd <benshep@me.com>
2015-04-27 12:16:38 +0100

    removed reload from wsgi

--- friarwood/wsgi.py
+++ friarwood/wsgi.py
@@ -6,13 +6,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
-import sys
-from imp import reload 
-
-reload(sys)     
-sys.setdefaultencoding("utf-8")
-
 import os
 
 #Heroku
