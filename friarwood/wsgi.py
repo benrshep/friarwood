e5798db815aefc339d6e3c96ebac90aa6ca3ac2e
Ben Shepherd <benshep@me.com>
2015-04-27 14:03:02 +0100

    static change

--- friarwood/wsgi.py
+++ friarwood/wsgi.py
@@ -12,6 +12,12 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "friarwood.settings")
 
 #application = get_wsgi_application()
