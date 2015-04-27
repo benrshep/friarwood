50d25f2ddc9ec83231ad6e3c11faa97b37f4f049
Ben Shepherd <benshep@me.com>
2015-04-27 14:11:35 +0100

    removed cling

--- friarwood/wsgi.py
+++ friarwood/wsgi.py
@@ -10,17 +10,20 @@ import os
 
 #Heroku
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
 
-from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "friarwood.settings")
+
 application = get_wsgi_application()
+
+#application = Cling(get_wsgi_application())
+
 application = DjangoWhiteNoise(application)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "friarwood.settings")
+
 
 #application = get_wsgi_application()
 
 #Heroku
-application = Cling(get_wsgi_application())
+
